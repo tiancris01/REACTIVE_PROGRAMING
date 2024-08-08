ed80b06825d77359c29babe81257549c8b416959
Daniel osorio <tiancris01@gmail.com>
2024-08-07 22:05:48 -0500

    chore: Refactor splash screen to use animated loading icon

--- lib/src/features/splash/widget/splash_screen.dart
+++ lib/src/features/splash/widget/splash_screen.dart
@@ -1,19 +1,70 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show pi;
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: 2 * pi,
+    ).animate(_controller);
+
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Icon(Icons.catching_pokemon, size: 100),
-              SizedBox(height: 20),
-              Text('loading...'),
+              AnimatedBuilder(
+                  animation: _controller,
+                  builder: (context, child) {
+                    return Transform(
+                      alignment: Alignment.center,
+                      transform: Matrix4.identity()..rotateY(_animation.value),
+                      child: const Icon(
+                        Icons.catching_pokemon,
+                        size: 100,
+                        color: Colors.blueAccent,
+                        shadows: [
+                          BoxShadow(
+                            color: Colors.black54,
+                            spreadRadius: 10,
+                            blurRadius: 3,
+                            offset: Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                    );
+                  }),
+              const SizedBox(height: 20),
+              const Text('loading...'),
             ],
           ),
         ),
